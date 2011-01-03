fa50035e2952a9ed866e9bcfc999c4358d988941
Curtis Carter <curtis@rubyhq.com>
2011-01-03 12:35:12 -0600

    Reverted to old way of loading factories. Fixes path issue

--- lib/factory_girl/rails2.rb
+++ lib/factory_girl/rails2.rb
@@ -1,1 +1,7 @@
-Rails.configuration.after_initialize { FactoryGirl.find_definitions }
+Rails.configuration.after_initialize {
+  Factory.definition_file_paths = [
+    File.join(Rails.root, 'test', 'factories'),
+    File.join(Rails.root, 'spec', 'factories')
+  ]
+  Factory.find_definitions
+}
