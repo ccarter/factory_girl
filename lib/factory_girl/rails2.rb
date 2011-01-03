9de18c1175ad3fbb4307a12b9a11b0cdfd230f76
Curtis Carter <curtis@rubyhq.com>
2011-01-03 12:49:02 -0600

    Changed Factory to FactoryGirl as Factory is deprecated

--- lib/factory_girl/rails2.rb
+++ lib/factory_girl/rails2.rb
@@ -1,7 +1,7 @@
 Rails.configuration.after_initialize {
-  Factory.definition_file_paths = [
+  FactoryGirl.definition_file_paths = [
     File.join(Rails.root, 'test', 'factories'),
     File.join(Rails.root, 'spec', 'factories')
   ]
-  Factory.find_definitions
+  FactoryGirl.find_definitions
 }
